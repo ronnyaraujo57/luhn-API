46f302c8aa3f523ee63cc82c34fa6e10056eab65
Ronny Araujo <ronnyaraujo57@gmail.com>
2020-12-12 23:22:31 -0400

    modificacion menor

--- src/index.js
+++ src/index.js
@@ -24,11 +24,12 @@ function Luhn(valor){
 			aux = 0;
 
 		if(aux === parseInt(valor[valor.length-1]))
-			return true;
+			return {"true","Aprobada."};
 		else
-			return false;
+			return {"false","Esta numeracion fue no paso el test del modulo 10."};
 	}
-	return false;
+	//return false;
+	return {"false","No coincide con la candidad de digitos de una cedula."};
 }
 
 //middleware
@@ -45,8 +46,13 @@ app.post('/', (req, res) =>{
 	console.log(req.body);
 
 	const { cedula } = req.body;
+	let respuesta = Luhn(cedula);
+	var data = {
+		"status": respuesta[0],
+		"mjs": respuesta[1]
+	}
 	if(cedula){
-		res.send(Luhn(cedula));
+		res.json(data);
 	}else{
 		req.send("Error");
 	}
